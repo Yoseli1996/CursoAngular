283fe8b9021028701e25376c874a5204ab8caec8
Yoseli1996 <yossitacondorir@gmail.com>
2022-11-23 21:08:06 -0400

    Versión1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+interface Tarjeta{
+  titulo: string;
+  subtitulo: string;
+  nro?: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit{
+  title = 'Angular Yoss';
+  public ArregloTarjetas: Tarjeta[]=[];
+
+  ngOnInit(): void {
+    this.ArregloTarjetas = [
+      {titulo:'Video1', subtitulo: 'subtitulo1'},
+      {titulo:'Video2', subtitulo: 'subtitulo2'},
+      {titulo:'Video3', subtitulo: 'subtitulo3'}
+    ]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { HeaderComponent } from './header/header.component';
+import { MyCardComponent } from './my-card/my-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    HeaderComponent,
+    MyCardComponent
   ],
   imports: [
     BrowserModule
